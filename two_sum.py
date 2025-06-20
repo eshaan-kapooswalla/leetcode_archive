809b89aee8e1618b52cccf2e57a4843bd4684b75
Eshaan Oozher Kapooswalla <eshaan.kapooswalla77@gmail.com>
2025-06-20 04:35:23 -0400

    Change Two Sum to brute-force O(n^2) solution for demonstration

--- two_sum.py
+++ two_sum.py
@@ -12,17 +12,10 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Create a hash map to store number -> index mapping
-        num_map = {}
-        
-        for i, num in enumerate(nums):
-            complement = target - num
-            
-            # If complement exists in map, we found our pair
-            if complement in num_map:
-                return [num_map[complement], i]
-                
-            # Store current number and its index
-            num_map[num] = i
-            
-        return []  # No solution found
+        # Brute-force O(n^2) solution
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
